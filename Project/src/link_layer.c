8f907dd075443c6e8cd638aa82fb3d8b7c704343
Oleksandr <53474466+M4rxman@users.noreply.github.com>
2024-10-17 20:01:32 +0100

    Update link_layer.c
    started to implement basic functionality of llopen method

--- Project/src/link_layer.c
+++ Project/src/link_layer.c
@@ -3,21 +3,99 @@
 #include "link_layer.h"
 #include "serial_port.h"
 
+#include <termios.h>
+#include <signal.h>
+#include <fcntl.h>
+
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define BUF_SIZE 2048
+#define CONTROL_BYTE_SIZE 5
+
+#define FLAG 0x7E
+#define A_tx 0x03 
+#define C_SET 0x03
+#define C_UA 0x07
+
+
+#define FALSE 0
+#define TRUE 1
+
+int alarmEnabled = FALSE;
+int alarmCount = 0;
+
+// Alarm function handler
+void alarmHandler(int signal)
+{
+    alarmEnabled = FALSE;
+    alarmCount++;
+
+    printf("Alarm #%d\n", alarmCount);
+}
 
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
 int llopen(LinkLayer connectionParameters)
-{
-    if (openSerialPort(connectionParameters.serialPort,
-                       connectionParameters.baudRate) < 0)
+{ 
+
+    int state = 0;
+    int fd = openSerialPort(connectionParameters.serialPort, connectionParameters.baudRate);
+    
+    if (fd < 0)
     {
         return -1;
     }
 
-    // TODO
+    unsigned char set[CONTROL_BYTE_SIZE];
+    unsigned char ua[CONTROL_BYTE_SIZE];
+    
+
+    set[0] = FLAG;
+    set[1] = A_tx;
+    set[2] = C_SET;
+    set[3] = set[1] ^ set[2];
+    set[4] = FLAG;
+
+    ua[0] = FLAG;
+    ua[1] = A_tx;
+    ua[2] = C_UA;
+    ua[3] = ua[1] ^ ua[2];
+    ua[4] = FLAG;
+
+    // Create string to send
+    unsigned char buf[CONTROL_BYTE_SIZE] = {0};
+
+    if (connectionParameters.role == LlTx) {
+    (void)signal(SIGALRM, alarmHandler);
+    
+        while (alarmCount < connectionParameters.nRetransmissions) {
+        
+            if (alarmEnabled == FALSE) {
+                write(fd, set, CONTROL_BYTE_SIZE);
+                alarmEnabled = TRUE;
+                alarm(connectionParameters.timeout); 
+                state = 0;
+            }
+            if (alarmCount == connectionParameters.nRetransmissions)
+                break;
+
+            read(fd, buf, 1);
+
+            //printf("var = 0x%02X state:%d\n", (unsigned int)(buf[0] & 0xFF), state);
+            changeOpenState(buf[0], &state, connectionParameters.role);
+
+            if (state == 5) {
+                alarm(0);
+                break;
+            }
+        }
+
+        printf("llopen\n");
+        return 1;
+    }
+
 
     return 1;
 }
